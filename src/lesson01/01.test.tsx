0d572834f92ab47a8d24986dbc442e6a05cca1b9
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-05-12 06:38:12 +0300

    Add test for func mult

--- src/lesson01/01.test.tsx
+++ src/lesson01/01.test.tsx
@@ -1,4 +1,4 @@
-import {sum} from "./01";
+import {mult, sum} from "./01";
 
 test('sum should be correct', () => {
 
@@ -14,5 +14,22 @@ test('sum should be correct', () => {
     //expect result
     expect(result1).toBe(3);
     expect(result2).toBe(5);
+
+})
+
+test('multiply should be correct', () => {
+
+    //data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    //action
+    const result1 = mult(a, b);
+    const result2 = mult(b, c);
+
+    //expect result
+    expect(result1).toBe(2);
+    expect(result2).toBe(6);
 
 })
