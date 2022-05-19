5808b7a41040a68f3d9edeba8604a1a9aebb1d7d
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-05-19 05:35:29 +0300

    Add test for func splitIntoWords

--- src/lesson01/01.test.tsx
+++ src/lesson01/01.test.tsx
@@ -1,4 +1,4 @@
-import {mult, sum} from "./01";
+import {mult, splitIntoWords, sum} from "./01";
 
 test('sum should be correct', () => {
 
@@ -33,3 +33,30 @@ test('multiply should be correct', () => {
     expect(result2).toBe(6);
 
 })
+
+test('split into words should be correct', () => {
+
+    //data
+    const sent1 = "Hello my friends";
+    const sent2 = "JS - the best programming language";
+    const c = 3;
+
+    //action
+    const result1 = splitIntoWords(sent1);
+    const result2 = splitIntoWords(sent2);
+
+    //expect result
+    expect(result1.length).toBe(3);
+    expect(result1[0]).toBe("Hello");
+    expect(result1[1]).toBe("my");
+    expect(result1[2]).toBe("friends");
+
+    expect(result2.length).toBe(5);
+    expect(result2[0]).toBe("JS");
+    expect(result2[1]).toBe("the");
+    expect(result2[2]).toBe("best");
+    expect(result2[2]).toBe("programming");
+    expect(result2[2]).toBe("lnguage");
+
+})
+
