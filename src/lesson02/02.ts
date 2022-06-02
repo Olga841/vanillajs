cf8f04d7ece1bf1240ec189f67e0d201b5e39686
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-06-02 06:05:53 +0300

    Add object 'student'

---
+++ src/lesson02/02.ts
@@ -0,0 +1,31 @@
+const student = {
+    id: 1,
+    name: "Dimych",
+    age: 27,
+    isActive: false,
+    adress: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: 'Minsk',
+            countryTitle: 'Belarus'
+        }
+    },
+    technolodgies: [
+        {
+            id: 1,
+            name: 'HTML'
+        },
+        {
+            id: 2,
+            name: 'CSS'
+        },
+        {
+            id: 3,
+            name: 'JS'
+        },
+        {
+            id: 4,
+            name: 'React'
+        }
+    ]
+}
